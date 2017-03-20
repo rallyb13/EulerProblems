04fc3420bacf7441a61133ef551813cdccabb53a
Benjamin Herson <benjaminherson@gmail.com>
2017-03-20 12:15:40 -0700

    add solution to problem #3

--- Math.js
+++ Math.js
@@ -13,6 +13,7 @@ multiples = function () {
 // cooler still, could take 3, 5, & 1000 as args
 // get the sum of any two multiples up to any number
 
+
 // Euler Problem #2
 // Sum of all even numbers in the Fibonacci sequence that are under 4mil
 fibEvens = function() {
@@ -36,3 +37,27 @@ fibEvens = function() {
 } // returns 4613732
 // I am the 523054th to solve this.
 // *could* && c < 4000000 for if, and use c for while condition--didn't need to
+
+
+// Euler Problem #3
+// Largest prime factor of 600851475143
+largestFactor = function(num) {
+  var keepFactoring = true,
+    bigNum = num,
+    lowFactor = 2,
+    checkLimit = Math.floor(Math.sqrt(num));
+  
+  while (lowFactor < checkLimit) {
+    if (bigNum % lowFactor === 0) {
+      bigNum = bigNum / lowFactor;
+      checkLimit = Math.floor(Math.sqrt(bigNum)); // I hate repeating this, but nowhere better 
+      // console.log(lowFactor); // 71, 839, 1471 if you're curious
+    } else {
+      ++lowFactor;
+    }
+  }
+  return bigNum;
+} // returns 6857
+// I am the 375972nd to solve this.
+// what I don't like about this is that I'm letting lowFactor be so many non-primes,
+// but eliminating lowFactor composites pretty much doubles the coding work
