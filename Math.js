53fc6904f46d98ee23cc60ce151fb5d4b6f6dbaa
Benjamin Herson <benjaminherson@gmail.com>
2017-03-20 21:36:43 -0700

    add fn to solve Euler#4 (with proto-attempt for notes)

--- Math.js
+++ Math.js
@@ -1,5 +1,5 @@
 // Euler Problem #1
-// Sum of all multiples of 3 and 5 between up to 1000 (non-inclusive)
+// Sum of all multiples of 3 and 5 up to 1000 (non-inclusive)
 multiples = function () {
   var sum = 0;
   for (var i=0; i < 1000; i++) {
@@ -61,3 +61,76 @@ largestFactor = function(num) {
 // I am the 375972nd to solve this.
 // what I don't like about this is that I'm letting lowFactor be so many non-primes,
 // but eliminating lowFactor composites pretty much doubles the coding work
+
+
+// Euler Problem #4: Proto-Attempt
+// Largest Palindrome created by multiplying 2 3-digit numbers
+highPal = function() {
+  var a = 999,
+    b = 999,
+    highestPal = 0,
+    prodString,
+    backHalf;
+  
+  // assumes a 6-digit palindrome will be found
+  while (highestPal === 0) {
+    prodString = String(a * b);
+    console.log(a);
+    backHalf = prodString.slice(3).split("").reverse().join("");
+    if (prodString.slice(0,3) === backHalf) {
+      highestPal = prodString;
+    } else {
+      if (a === b) {
+        a = 999;
+        b = b - 1;
+      } else {
+        a = a - 1
+      }
+    }
+  }
+  return highestPal;
+}
+// first alternated subtracting from b & a [got 698896, which is 836 squared], realized I'd skip 999 * 997
+// so switched to still imperfect drop b after each square, have a move from 999 to where b is
+// [got 888888 (962 * 924)], but even this would check 997 * 996 before 999 * 995
+
+
+// Euler Problem #4: Done Right
+// Largest Palindrome created by multiplying 2 3-digit numbers
+highPal = function() {
+  var a = 999,
+    b = 999,
+    lowestA = 999,
+    highestPal = 0,
+    prodString,
+    backHalf
+    startFromSquare = true;
+  
+  // assumes a 6-digit palindrome will be found
+  while (highestPal === 0) {
+    prodString = String(a * b);
+    console.log(a);
+    backHalf = prodString.slice(3).split("").reverse().join("");
+    if (prodString.slice(0,3) === backHalf) {
+      highestPal = prodString;
+    } else { // we need to get next equation
+      if (a === 999) { // at end of diagonal, need to start next
+        startFromSquare = !startFromSquare;
+        if (startFromSquare === true) { // resets to next sqaure: highest pinnacle
+          --lowestA;
+          a = lowestA;
+          b = lowestA;
+        } else { // resets to left of pinnacle, for parallel diagonal
+          a = lowestA;
+          b = lowestA - 1;
+        }
+      } else { // just slide to next equation along diagonal
+        ++a;
+        --b;
+      }
+    }
+  }
+  return highestPal;
+} // returns 906609 (993 x 913)
+// I am the 336435th to solve this
+// that took some real thinking about the times tables and how to get the exact next highest product!
