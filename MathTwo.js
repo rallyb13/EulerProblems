bff16d3360fe816592b6bed8ad39a2b126f2aaaa
Benjamin Herson <benjaminherson@gmail.com>
2017-04-27 19:00:06 -0700

    add solution for Euler #27

--- MathTwo.js
+++ MathTwo.js
@@ -49,3 +49,73 @@ unitFract = function() {
   return denom;
 } // returns 983 (982-digit cycle!)
 // I am the 62095th to solve this
+
+
+// Euler #27
+// product of coefficients a & b of n^2 +an + b that produces most consecutive values that are primes
+// starting with n=0 (a & b are both between -1000 & 1000)
+quadPrime = function() {
+  var primes = [2, 3],
+    checkNum = 2,
+    a = -999,
+    b = -1000,
+    n = 0,
+    best = [40, 0, 41],
+    solution = 0,
+    checkLimit = 0,
+    solPrime = true,
+    candidatePrime = true,
+    i = 0;
+    
+    while (a < 1000) {
+      solPrime = true;
+      while (solPrime === true) {
+        solution = Math.abs((n * n) + (a * n) + b);
+        checkLimit = Math.floor(Math.sqrt(solution));
+        i = 0;
+        checkNum = primes[0];
+        while (checkNum <= checkLimit && solPrime === true) {
+          if (solution % checkNum === 0) {
+            solPrime = false;
+          }
+          ++i;
+          
+          // record next prime if run out (even if just to know it can be skipped)
+          while (typeof primes[i] === 'undefined') {
+            candidatePrime = true;
+            checkNum += 2;
+            for (var j=0; j<primes.length; j++) {
+              if (checkNum % primes[j] === 0) {
+                candidatePrime = false;
+                break;
+              }
+            }
+            if (candidatePrime === true) {
+              primes.push(checkNum);
+            }
+          }
+          checkNum = primes[i];
+        }
+        if (solPrime === true) {
+          ++n;
+        }
+      }
+      if (n > best[0]) {
+        best = [n, a, b];
+        console.log(best);
+      }
+      
+      n = 0;
+      if (b !== 1000) {
+        ++b;
+      } else {
+        b = -1000;
+        ++a;
+      }
+    }
+    
+    return best[1] * best[2];
+} // returns -59231 (71 consecutive numbers; a = -61; b = 971)
+// I am the 65059th to solve this
+// Note: surprised how fast the solution popped for that complexity!
+
